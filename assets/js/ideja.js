81a97b8dd7dddfc54c7054c3f7b3850b426b8c96
diana1405 <106436951+diana1405@users.noreply.github.com>
2022-07-04 02:49:25 +0300

    Add files via upload

--- assets/js/ideja.js
+++ assets/js/ideja.js
@@ -1,3 +1,38 @@
 $('.nav-toggle').on('click', function(){
     $('#menu').toggleClass('active');
   });
+
+
+if ($(window).width() < 870) {
+    document.getElementById("java").src="assets/img/ideja/Juris2.png";
+}
+ else {
+    document.getElementById("java").src="assets/img/ideja/Juris.png";
+}
+
+$(window).resize(function() {
+  if ($(window).width() < 870) {
+    document.getElementById("java").src="assets/img/ideja/Juris2.png";
+  }
+ else {
+    document.getElementById("java").src="assets/img/ideja/Juris.png";
+ }
+});
+
+
+
+if ($(window).width() < 510) {
+  document.getElementById("java").src="assets/img/ideja/Juris_mobile.png";
+}
+else {
+  document.getElementById("java").src="assets/img/ideja/Juris2.png";
+}
+
+$(window).resize(function() {
+if ($(window).width() < 510) {
+  document.getElementById("java").src="assets/img/ideja/Juris_mobile.png";
+}
+else {
+  document.getElementById("java").src="assets/img/ideja/Juris2.png";
+}
+});
