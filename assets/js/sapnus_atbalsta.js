a9956d2b991daf9fc5cbb6e6202ab749e67e023f
diana1405 <106436951+diana1405@users.noreply.github.com>
2022-07-25 04:30:18 +0300

    Add files via upload

--- assets/js/sapnus_atbalsta.js
+++ assets/js/sapnus_atbalsta.js
@@ -1,3 +1,43 @@
 $('.nav-toggle').on('click', function(){
     $('#menu').toggleClass('active');
   });
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: 56.94, lng: 24.10 };
+  
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 13,
+    center: uluru,
+    mapTypeControl: false,
+    streetViewControl: false,
+    fullscreenControl: false,
+    mapId: '5aee6f55666babb5',
+  });
+
+  const popupContent = '<div class="karta-text"><div><h1>AUCH</h1><p>beauty home</p></div><div><h2>AUCH beauty home</h2><p>+371 28361686, +371 23202079<br>auchbeauty@gmail.com<br>Cēsu iela 20, Rīga</p></div></div>';
+  const image = "assets/img/sapnus_atbalsta/yellow-marker.png";
+  
+
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+    title: "Hello World!",
+    icon: image,
+  });
+
+  infowindow = new google.maps.InfoWindow({
+    content: popupContent,
+  });
+
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  });
+  
+
+}
+
+window.initMap = initMap;
