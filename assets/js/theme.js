6f9f4857a9a0a4f9bccb4ce48b4dcfc42a8750c7
diana1405 <106436951+diana1405@users.noreply.github.com>
2022-07-25 10:32:01 +0300

    Add files via upload

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,3 +1,36 @@
 $('.nav-toggle').on('click', function(){
     $('#menu').toggleClass('active');
   });
+
+// COOKIE
+function setCookie(name, value, days) {
+  let expires = "";
+  if (days) {
+      let date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+
+function checkCookies() {
+  let cookieNote = document.getElementById('cookie_note');
+  let cookieBtnAccept = cookieNote.querySelector('.cookie_accept');
+
+  if (!getCookie('cookies_policy')) {
+      cookieNote.classList.add('show');
+  }
+
+  cookieBtnAccept.addEventListener('click', function () {
+      setCookie('cookies_policy', 'true', 365);
+      cookieNote.classList.remove('show');
+  });
+}
+
+checkCookies();
